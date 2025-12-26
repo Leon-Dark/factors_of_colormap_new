92c498c0e2483a748d7ab48040e21c70a2073915
Leon-Dark <101171383+Leon-Dark@users.noreply.github.com>
2025-12-26 15:44:23 +0800

    update

--- random/gaussian_perturbation_system/js/gaussianGenerator.js
+++ random/gaussian_perturbation_system/js/gaussianGenerator.js
@@ -185,12 +185,12 @@ class GaussianGenerator {
             }
         }
 
-        // Apply logarithmic compression
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                field[y][x] = Math.log(1 + field[y][x]);
-            }
-        }
+        // // Apply logarithmic compression
+        // for (let y = 0; y < height; y++) {
+        //     for (let x = 0; x < width; x++) {
+        //         field[y][x] = Math.log(1 + field[y][x]);
+        //     }
+        // }
         
         return field;
     }
